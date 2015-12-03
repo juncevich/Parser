662eade09da219ae3e32af0ab2229e17834728c6
Alex <alexunc@mail.ru>
2015-12-03 23:29:32 +0500

    Add JavaFx menu> not  good design.

---
+++ src/main/java/GUI/Main.java
@@ -0,0 +1,58 @@
+package GUI;
+
+import com.sun.javafx.geom.Vec2f;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import logic.Parser;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.Random;
+
+
+public class Main extends Application {
+    Parser parser;
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        parser = new Parser();
+        primaryStage.setTitle("Hello World");
+        Button button = new Button("click me");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        VBox vBox = new VBox();
+
+        for (Element element : parser.getCategoryList()
+                ) {
+            vBox.getChildren().add(new Button(element.text()));
+
+
+        }
+
+
+        primaryStage.setScene(new Scene(vBox,300,300));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
