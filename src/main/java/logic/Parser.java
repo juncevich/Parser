b2b00648d0c811be53419f03c7777326271c5bcf
Alex <alexunc@mail.ru>
2015-12-09 00:54:13 +0500

    Получил аттрибут ссылки.

--- src/main/java/logic/Parser.java
+++ src/main/java/logic/Parser.java
@@ -15,45 +15,60 @@ public class Parser {
     String title = null;
     static String URL = "http://ekb.zarplata.ru/";
 
-    public Parser() throws IOException {
+    public Parser() {
+    getMenuList();
+    getResumeList();
 
 
-    document = Jsoup.connect(URL).get();
-    title = document.title();
-//            Elements mainHeaderElements = document.select("nav.ra-rubricator-entity-categories");
-//            for (Element element: mainHeaderElements
-//                 ) {
-//                System.out.println(element);
-//            }
 
-        //Elements mainHeaderElements1 = document.select("li.ra-rubricator-entity-item");
-    categoryList = document.select("a[data-analytic=\"clickRubricatorResume\"]");
+
+
+}
+    public void getMenuList(){
+        try {
+            document = Jsoup.connect(URL).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        title = document.title();
+
+        categoryList = document.select("a[data-analytic=\"clickRubricatorResume\"]");
 
 
         for (Element element : categoryList
                 ) {
             System.out.println(element);
-            System.out.println(element.attr("href"));
+            System.out.println(element.attr("abs:href"));
             //System.out.println(element.text());
         }
-        //Elements title1 = doc.select("<document>");
-        //Elements title1 = document.select("body");
-        //System.out.println(title1.get(1));
-        //System.out.println(title1.toString());
+
         System.out.println(title);
-
-
-//        System.out.println("Jsoup Can read HTML page from URL, title : " + title);
-
-}
-    public Elements getResumeList(String categoryName){
+    }
+    public void getResumeList(){
         try {
-            document = Jsoup.connect(URL + categoryName).get();
+            document = Jsoup.connect("http://ekb.zarplata.ru/resume?state%5B%5D=1&visibility_type=1&average_salary=1&categories_facets=1&city_id%5B%5D=994&rubric%5B%5D=1&limit=25").get();
+            //document = Jsoup.connect(categoryList.get(0).attr("abs:href")).timeout(0).get();
+            //System.out.println(document.toString());
+            //document = Jsoup.connect(categoryList.get(0).attr("abs:href")).data("state[]","1").data("visibility_type","1").data("average_salary","1").data("categories_facets","1").data("city_id[]","994").data("rubric[]","1").data("limit", "25").data("offset","50").post();
+            //document = Jsoup.parse(categoryList.get(0).attr("abs:href"), 10000);
+            //title = document.title();
+            //resumeList = document.select("div[data-type=\"resumes\"]");
+            //resumeList = document.select("a").attr("class","ra-elements-list__card__title");
+            //resumeList = document.select("ul").attr("class", "ra-elements-list-hidden");
+           //resumeList = document.select("div").attr("data-type", "resumes").select("li");
+           resumeList = document.select("div").attr("class","ra-elements-list__item").attr("data-id", "39140625").attr("data-analytic", "");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (Element element : resumeList
+                ) {
+           System.out.println(element);
+            //System.out.println(element.attr("abs:href"));
+            //System.out.println(element.text());
+        }
 
-        return resumeList;
+
     }
     public static Elements getCategoryList() {
         return categoryList;
