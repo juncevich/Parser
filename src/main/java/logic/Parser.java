6e4f92e95ac0ab39021956446e47b10c1735323c
Alex <a.juncevich@gmail.com>
2015-12-09 12:29:25 +0500

    Нашел способ получить json с необходимой информацией.

--- src/main/java/logic/Parser.java
+++ src/main/java/logic/Parser.java
@@ -46,8 +46,8 @@ public class Parser {
     }
     public void getResumeList(){
         try {
-             document = Jsoup.connect("http://ekb.zarplata.ru/resume?state%5B%5D=1&visibility_type=1&average_salary=1&categories_facets=1&city_id%5B%5D=994&rubric%5B%5D=1&limit=25").timeout(1000000)
-                    .header("Accept", "text/javascript")
+             document = Jsoup.connect("http://ekb.zarplata.ru/api/v1/resumes/?state%5B%5D=1&visibility_type=1&average_salary=true&categories_facets=1&city_id%5B%5D=994&rubric%5B%5D=1&limit=25&offset=0&fields=add_date%2Cmod_date%2Cage_pretty%2Cattachment%2Cbirthday%2Ccontact%2Ceducation%2Ceducation_specialty%2Cexperience%2Cexperience_length%2Cexperience_prune%2Cheader%2Cid%2Cinfo%2Cis_upped%2Cis_journey%2Cjobs%2Cowner_id%2Cphoto%2Csalary%2Cstate%2Curl%2Cvalidate_state%2Cviews%2Chide_birthday%2Csex").timeout(1000000)
+                    .header("Accept", "text/*").ignoreContentType(true)
                     .userAgent("Mozilla/5.0 (Windows NT 6.1; rv:40.0) Gecko/20100101 Firefox/40.0")
                     .get()
                     ;
