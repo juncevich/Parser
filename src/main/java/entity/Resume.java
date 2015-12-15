622c0820c8fa83f1d7bddac033acd922c09453c9
Alex <a.juncevich@gmail.com>
2015-12-15 11:00:28 +0500

    Delete old comment.

--- src/main/java/entity/Resume.java
+++ src/main/java/entity/Resume.java
@@ -1,7 +1,6 @@
 package entity;
 
-/**
- * Created by Juncevich on 14.12.2015.
- */
+
 public class Resume {
+
 }
--- src/main/java/logic/Parser.java
+++ src/main/java/logic/Parser.java
@@ -22,9 +22,6 @@ public class Parser {
     getResumeList();
 
 
-
-
-
 }
     private void getMenuList(){
         try {
@@ -41,7 +38,7 @@ public class Parser {
                 ) {
             System.out.println(element);
             System.out.println(element.attr("abs:href"));
-            //System.out.println(element.text());
+
         }
 
         System.out.println(title);
@@ -95,28 +92,12 @@ public class Parser {
                 System.out.println("------------------------------------------------------------------------");
 
             }
-            //document = Jsoup.connect("http://ekb.zarplata.ru/resume?state%5B%5D=1&visibility_type=1&average_salary=1&categories_facets=1&city_id%5B%5D=994&rubric%5B%5D=1&limit=25").get();
-            //document = Jsoup.connect(categoryList.get(0).attr("abs:href")).timeout(0).get();
-            //System.out.println(document.toString());
-            //document = Jsoup.connect(categoryList.get(0).attr("abs:href")).data("state[]","1").data("visibility_type","1").data("average_salary","1").data("categories_facets","1").data("city_id[]","994").data("rubric[]","1").data("limit", "25").data("offset","50").post();
-            //document = Jsoup.parse(categoryList.get(0).attr("abs:href"), 10000);
-            //title = document.title();
-            //resumeList = document.select("div[data-type=\"resumes\"]");
-            //resumeList = document.select("a").attr("class","ra-elements-list__card__title");
-            //resumeList = document.select("ul").attr("class", "ra-elements-list-hidden");
-           //resumeList = document.select("div").attr("data-type", "resumes").select("li");
-           //resumeList = document.select("div").attr("class","ra-elements-list__item").attr("data-id", "39140625").attr("data-analytic", "");
+
 
         }
         catch (IOException e) {
             e.printStackTrace();
         }
-//        for (Element element : resumeList
-//                ) {
-//           System.out.println(element);
-//            //System.out.println(element.attr("abs:href"));
-//            //System.out.println(element.text());
-//        }
 
 
     }
