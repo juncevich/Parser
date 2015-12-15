75009bb5788496aa8536b018d66c9909b62b7e4a
Alex <a.juncevich@gmail.com>
2015-12-15 16:35:43 +0500

    Раскидал GUI на CategoryMenu & ResumeList.

--- src/main/java/GUI/ResumeList.java
+++ src/main/java/GUI/ResumeList.java
@@ -2,24 +2,27 @@ package gui;
 
 import javafx.beans.NamedArg;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 
 import java.io.IOException;
 
 
-public class ResumeList extends Scene {
-    Parent loader;
-
-    public ResumeList(@NamedArg("root") Parent loader) {
-        super(loader);
+public class ResumeList extends AnchorPane {
+    /**
+     * Creates an AnchorPane layout.
+     */
+    public ResumeList() {
         try {
-            this.loader = FXMLLoader.load(getClass().getResource("ResumeList.fxml"));
+            getChildren().add(FXMLLoader.<Node>load(ClassLoader.getSystemResource("ResumeListView.fxml")));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-        this.loader = loader;
     }
-
 }
